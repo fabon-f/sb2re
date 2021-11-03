67c361b223441cc87fad860f87b9d8c9f599b4fe
fabon <syobon.hinata.public@gmail.com>
2021-11-03 14:31:50 +0900

    Impleent basic convertion of inline elements

--- mod.ts
+++ mod.ts
@@ -21,6 +21,8 @@ function generateReView(ast: scrapboxParser.Page, option: ReViewOption = {}): st
                 if (boldNode.nodes[0].type !== "plain") { throw new Error("inside header") }
                 out += `${header} ${boldNode.nodes[0].text}`;
                 out += "\n";
+            } else {
+                out += n.nodes.map(nodeToReView).join("");
             }
         }
     }
@@ -28,7 +30,46 @@ function generateReView(ast: scrapboxParser.Page, option: ReViewOption = {}): st
     return out;
 }
 
-export default function scrapboxToReView(src: string, option?: ReViewOption): string {
-    const ast = scrapboxParser.parse(src);
+function nodeToReView(node: scrapboxParser.Node): string {
+    if (node.type === "link") {
+        if (node.pathType === "relative") {
+            console.error(`Can't convert relative links. Please use absolute links instead: ${node.raw}`);
+            return node.raw;
+        }
+        if (node.pathType === "root") {
+            console.info(`An internal link to a Scrapbox's page is used: ${node.raw}`);
+            const href = new URL(node.href, "https://scrapbox.io").href;
+            return `@<href>{${href}}`;
+        }
+        return node.content === "" ? `@<href>{${node.href}}` : `@<href>{${node.href}, ${node.content}}`;
+    } else if (node.type === "hashTag") {
+        console.error(`Can't convert relative links. Please use absolute links instead: ${node.raw}`);
+        return node.raw;
+    } else if (node.type === "strong") {
+        return `@<strong>{${node.nodes.map(nodeToReView).join("")}}`;
+    } else if (node.type === "decoration") {
+        return node.decos.reduce((inside, decoration) => {
+            if (/\*-[0-9]*/.test(decoration)) {
+                return `@<strong>{${inside}}`;
+            } else if (decoration === "/") {
+                return `@<i>{${inside}}`;
+            } else if (decoration === "-") {
+                return `@<del>{${inside}}`;
+            }
+            return inside;
+        }, node.nodes.map(nodeToReView).join(""));
+    } else if (node.type === "code") {
+        return `@<code>{${node.text}}`;
+    } else if (node.type === "formula") {
+        return `@<m>{${node.formula}}`;
+    } else if (node.type === "plain") {
+        return node.text;
+    } else {
+        return node.raw;
+    }
+}
+
+export default function scrapboxToReView(src: string, option: ReViewOption & scrapboxParser.ParserOption = {}): string {
+    const ast = scrapboxParser.parse(src, option);
     return generateReView(ast, option);
 }
--- test/test.ts
+++ test/test.ts
@@ -24,3 +24,33 @@ Deno.test("Heading with baseHeadingLevel", () => {
 `;
     assertEquals(scrapboxToReView(sbContent, { baseHeadingLevel: 4 }), reviewCode)
 });
+
+Deno.test("Decoration", () => {
+    const sbContent = "[[Bold]][* Bold][**/- BoldStrikeItalic]";
+    const reviewCode = "@<strong>{Bold}@<strong>{Bold}@<strong>{@<del>{@<i>{BoldStrikeItalic}}}";
+    assertEquals(scrapboxToReView(sbContent, { hasTitle: false }), reviewCode);
+});
+
+Deno.test("External Link", () => {
+    const sbContent = "https://google.com [https://google.com] [https://google.com G] [G https://google.com]"
+    const reviewCode = "@<href>{https://google.com} @<href>{https://google.com} @<href>{https://google.com, G} @<href>{https://google.com, G}";
+    assertEquals(scrapboxToReView(sbContent, { hasTitle: false }), reviewCode);
+});
+
+Deno.test("Link across projects", () => {
+    const sbContent = "[/projectname][/projectname/pagename]";
+    const reviewCode = "@<href>{https://scrapbox.io/projectname}@<href>{https://scrapbox.io/projectname/pagename}";
+    assertEquals(scrapboxToReView(sbContent, { hasTitle: false }), reviewCode);
+});
+
+Deno.test("Inline code", () => {
+    const sbContent = "`code`";
+    const reviewCode = "@<code>{code}";
+    assertEquals(scrapboxToReView(sbContent, { hasTitle: false }), reviewCode);
+});
+
+Deno.test("Formula", () => {
+    const sbContent = "[$ E = mc^2]";
+    const reviewCode = "@<m>{E = mc^2}";
+    assertEquals(scrapboxToReView(sbContent, { hasTitle: false }), reviewCode);
+});
